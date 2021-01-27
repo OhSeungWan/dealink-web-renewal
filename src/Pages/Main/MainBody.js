51bf563c14811d27595471f9e059170aadb80ead
OhSeungWan <tmddhks4717@naver.com>
2021-01-27 17:28:39 +0900

    [mod] modified pages/main
    modified main for renewal
    기존 상품등록 화면과 기능은 동일

---
+++ src/Pages/Main/MainBody.js
@@ -0,0 +1,7 @@
+import AuctionRegister from 'system/Auction/AuctionRegister';
+import React from 'react';
+const MainBody = () => {
+  return <AuctionRegister />;
+};
+
+export default MainBody;
---
+++ src/Pages/Main/MainHeader.js
@@ -0,0 +1,8 @@
+import Header from 'Components/Molecules/Header';
+import React from 'react';
+
+const MainHeader = () => {
+  return <Header />;
+};
+
+export default MainHeader;
