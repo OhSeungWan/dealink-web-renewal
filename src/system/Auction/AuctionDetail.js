72e58731b77b9e38198544f05fdfbd6ac74a09a7
OhSeungWan <tmddhks4717@naver.com>
2021-02-05 18:41:12 +0900

    [mod] modified Auction System
    필터 추가 , 타이머 렌더링 오류 수정, 스크롤 이벤트 수정

---
+++ src/system/Auction/AuctionDetail.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { getCookie, setCookie } from 'lib/Cookies';
+import { useLocation, useParams } from 'react-router-dom';
+
+import { useSelector } from 'react-redux';
+
+const AuctionDetail = () => {
+  const { url } = useParams();
+  const location = useLocation();
+
+  const [firstTime, setFirstTime] = useState(true);
+  const [isSee, setIsSee] = useState(getCookie('isSee'));
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [data, setData] = useState({ auctionDetail: {}, loading: false });
+
+  const userId = useSelector(state =>
+    state.user.id !== 'undefined' && state.user.id !== 'null' && state.user.id
+      ? userInfo.id
+      : '0'
+  );
+
+  const closeFirstModal = () => {
+    setFirstTime(false);
+  };
+
+  const doNotSeeToday = () => {
+    setIsSee(true);
+    setCookie('isSee', 'true', 1);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+    // fetchData();
+  };
+
+  const openModal = () => {
+    setCookie('before', location.pathname, 1);
+    setIsOpen(true);
+  };
+
+  const getAuctionDetail = async () => {
+    setData(state => ({ ...state, loading: false }));
+    const res = await fetch(
+      `https://rest.dealink.co.kr/user/${userId}/auction/${url}`
+    );
+    const data = await res.json();
+    setData(state => ({ ...state, auctionDetail: data, loading: true }));
+  };
+
+  return <div></div>;
+};
+
+export default AuctionDetail;
--- src/system/Auction/AuctionList.js
+++ src/system/Auction/AuctionList.js
@@ -6,18 +6,35 @@ import styled from 'styled-components';
 const AuctionList = () => {
   const [auctionList, setAuctionList] = useState([]);
   const [page, setPage] = useState(0);
+  const [sortKey, setSortKey] = useState('createdDate');
   const pageStateRef = useRef(page);
 
   const setPageState = data => {
     pageStateRef.current = data;
     setPage(data);
   };
-
-  const getAuctionList = async () => {
+  const handleChageSortKey = async e => {
+    const SortKey = {
+      new: 'createdDate',
+      closing: 'closingTime',
+      price: 'currentPrice'
+    };
+    const { name } = e.target;
+    setSortKey(SortKey[name]);
     const res = await fetch(
-      `https://rest.dealink.co.kr/auction/list?page=${pageStateRef.current}&size=20&sort=closingTime,ASC`
+      `https://rest.dealink.co.kr/auction/list?page=0&size=20&sort=${SortKey[name]},ASC`
     );
     const data = await res.json();
+    setAuctionList(data.content);
+  };
+
+  const getAuctionList = async () => {
+    console.log(pageStateRef.current);
+    const res = await fetch(
+      `https://rest.dealink.co.kr/auction/list?page=${pageStateRef.current}&size=20&sort=${sortKey},ASC`
+    );
+    const data = await res.json();
+    setPageState(pageStateRef.current + 1);
     setAuctionList(list => list.concat(data.content));
   };
 
@@ -44,19 +61,50 @@ const AuctionList = () => {
     };
   }, []);
 
-  useEffect(() => {
-    setPageState(page + 1);
-  }, [auctionList]);
   return (
-    <AuctionListWrapper>
-      {auctionList.map(auction => {
-        return <AuctionItem auction={auction} />;
-      })}
-    </AuctionListWrapper>
+    <>
+      <FilterWrapper className="filter-wrapper">
+        <button name="new" onClick={handleChageSortKey}>
+          최신 순서
+        </button>
+        <button name="closing" onClick={handleChageSortKey}>
+          마감 순서
+        </button>
+        <button name="price" onClick={handleChageSortKey}>
+          가격 순서
+        </button>
+      </FilterWrapper>
+      <AuctionListWrapper>
+        {auctionList.map((auction, index) => {
+          return <AuctionItem auction={auction} key={index} loading={index} />;
+        })}
+      </AuctionListWrapper>
+    </>
   );
 };
 
 export default AuctionList;
+
+const FilterWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 10px;
+  button {
+    border: none;
+    padding: 10px 25px;
+    border-radius: 5px;
+    background-color: #f5f5f7;
+    &:active {
+      background: gray;
+    }
+
+    &:focus {
+      background-color: black;
+      color: white;
+    }
+  }
+`;
 
 const AuctionListWrapper = styled.div`
   display: flex;
